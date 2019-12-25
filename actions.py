73805d335aa259b863130a36db7b2744c9e1d601
Teagan glenn <teagan.m.glenn@gmail.com>
2019-12-25 13:30:25 -0700

    Current weather

--- actions.py
+++ actions.py
@@ -94,3 +94,40 @@ class ActionWhoHome(Action):
             dispatcher.utter_message(template="utter_who_is_home", people=people_home)
 
         return []
+
+
+class ActionWhoHome(Action):
+
+    def __init__(self):
+        self.bearer_token = HOME_ASSISTANT_TOKEN
+
+    def name(self) -> Text:
+        return "action_current_weather"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        response = requests.get(
+            f"https://automation.prettybaked.com/api/states/weather.dark_sky_hourly",
+            headers={
+                "Authorization": f"Bearer {self.bearer_token}"
+            }
+        )
+        weather = None
+
+        try:
+            response.raise_for_status()
+            condition = response.json().get('state', None)
+            temperature = response.json().get('attributes', {}).get('temperature', None)
+            weather = "it's currently %s and %s" % (
+                condition, (temperature + " degrees") if temperature else "")
+        except requests.HTTPError as err:
+            _LOGGER.error(str(err))
+        if not weather:
+            dispatcher.utter_message(template="utter_noone_home")
+        else:
+            dispatcher.utter_message(text=weather)
+
+        return []
