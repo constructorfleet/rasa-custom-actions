ac50e0d85cc9b88ae38b0296293378e956c9009c
Teagan glenn <teagan.m.glenn@gmail.com>
2020-01-05 13:12:52 -0700

    Logs

--- media/tautalli.py
+++ media/tautalli.py
@@ -27,6 +27,7 @@ def get_recent_media(dispatcher: CollectingDispatcher,
                      tracker: Tracker,
                      domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
     media_type = next(tracker.get_latest_entity_values(MEDIA_TYPE_SLOT), None)
+    _LOGGER.warning(json.dumps(media_type))
     media_type = SYNONYMS.get(media_type, None)
     success, recent_media = query_recent_media(media_type)
     _LOGGER.warning(json.dumps(recent_media))
