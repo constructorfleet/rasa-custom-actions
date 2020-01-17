88b8133613fb08856a2fdcc96a9d4efe18b84272
Teagan glenn <teagan.m.glenn@gmail.com>
2020-01-16 18:32:38 -0700

    Add slots

--- third_party/__init__.py
+++ third_party/__init__.py
@@ -5,6 +5,8 @@ PAPRIKA_USERNAME = os.environ.get('PAPRIKA_USERNAME', '')
 PAPRIKA_PASSWORD = os.environ.get('PAPRIKA_PASSWORD', '')
 
 RECIPE_CATEGORIES_SLOT = 'recipe_categories'
+RECIPE_INGREDIENTS_SLOT = 'recipe_ingredients'
+RECIPE_DIRECTIONS_SLOT = 'recipe_directions'
 RECIPE_DURATION_SLOT = 'recipe_duration'
 RECIPE_NAME_LIKE_SLOT = 'recipe_name_like'
 
--- third_party/paprika.py
+++ third_party/paprika.py
@@ -13,7 +13,9 @@ from . import (
     RECIPE_CATEGORIES_SLOT,
     RECIPE_DURATION_SLOT,
     RECIPE_NAME_SLOT,
-    RECIPE_NAME_LIKE_SLOT
+    RECIPE_NAME_LIKE_SLOT,
+    RECIPE_DIRECTIONS_SLOT,
+    RECIPE_INGREDIENTS_SLOT
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -36,13 +38,17 @@ def get_a_recipe(client: Pyprika,
             name_like=name
         )
 
-        recipe_name = recipes[randint(0, len(recipes) - 1)] \
+        recipe = recipes[randint(0, len(recipes) - 1)] \
             if recipes and len(recipes) > 0 else None
-        if not recipe_name:
+        if not recipe:
             dispatcher.utter_message(template="utter_recipe_not_found")
         else:
-            dispatcher.utter_message(template="utter_recipe_name", recipe=recipe_name)
-        return [SlotSet(RECIPE_NAME_SLOT, recipe_name)]
+            dispatcher.utter_message(template="utter_recipe_name", recipe=recipe.name)
+            return [
+                SlotSet(RECIPE_NAME_SLOT, recipe.name),
+                SlotSet(RECIPE_INGREDIENTS_SLOT, recipe.ingredients),
+                SlotSet(RECIPE_DIRECTIONS_SLOT, recipe.directions)
+            ]
     except Exception as err:
         _LOGGER.warning(str(err))
         _LOGGER.error(traceback.print_exc(file=sys.stdout))
